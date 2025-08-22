6827fa34060206db29dd1f73ad54812bb4cf9ac1
Shabicha Sureshkumar <shabichasureshkumar@gmail.com>
2025-08-21 20:03:51 -0400

    Initial Commit

---
+++ messagessampleUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  messagessampleUI
+//
+//  Created by Shabicha Sureshkumar on 2025-08-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ messagessampleUITests/messagessampleUITests.swift
@@ -0,0 +1,17 @@
+//
+//  messagessampleUITests.swift
+//  messagessampleUITests
+//
+//  Created by Shabicha Sureshkumar on 2025-08-21.
+//
+
+import Testing
+@testable import messagessampleUI
+
+struct messagessampleUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
