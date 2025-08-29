6f2a0d35e6f756b21ee6565578963a45c6ff18da
Shabicha Sureshkumar <shabichasureshkumar@gmail.com>
2025-08-28 20:20:41 -0400

    split button

--- messagessampleUI/ViewController.swift
+++ messagessampleUI/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     private var billTitle = UITextField()
     private var amount = UILabel()
     private var splitBy = UILabel()
+    private var percent = UIButton()
     
     private var numField: UITextField = {
         let tf = UITextField()
@@ -29,7 +30,7 @@ class ViewController: UIViewController {
           tf.textAlignment = .left
         
           
-        // Create a container view for the $ sign
+        // container view for the $ sign
           let leftContainer = UIView(frame: CGRect(x: 0, y: 0, width: 35, height: 20))
           let dollarLabel = UILabel(frame: CGRect(x: 12, y: 0, width: 20, height: 20))
           dollarLabel.text = "$"
@@ -41,11 +42,22 @@ class ViewController: UIViewController {
           tf.leftView = leftContainer
           tf.leftViewMode = .always
            
-           // Optional: Add right padding too
            tf.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 55, height: 20))
            tf.rightViewMode = .always
         return tf
     } ()
+    
+    private var isSelected = false
+    @objc func toggleButton() {
+            isSelected.toggle()
+            
+            if isSelected {
+                percent.layer.borderWidth = 3.0
+                percent.layer.borderColor = UIColor.black.cgColor
+            } else {
+                percent.layer.borderWidth = 0
+            }
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,22 +87,22 @@ class ViewController: UIViewController {
         view.addSubview(appName)
         appName.translatesAutoresizingMaskIntoConstraints = false
         appName.heightAnchor.constraint(equalToConstant: 20.4).isActive = true
-        appName.leadingAnchor.constraint(equalTo: logoView.trailingAnchor, constant: 8).isActive = true // 8pt gap after logo
+        appName.leadingAnchor.constraint(equalTo: logoView.trailingAnchor, constant: 8).isActive = true
         appName.centerYAnchor.constraint(equalTo: logoView.centerYAnchor).isActive = true
         
         // bill name
         billTitle.frame = CGRect(x: 0, y: 0, width: 154.4, height: 34.2)
         billTitle.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
         billTitle.font = UIFont(name: ".NewYorkLarge-Bold", size: 32)
-        billTitle.text = "Add Your Bill Title" // Set the actual text content
-        billTitle.placeholder = "Bill Title" // Optional placeholder
-        billTitle.borderStyle = .none // Remove default border if you want
+        billTitle.text = "Add Your Bill Title"
+        billTitle.placeholder = "Bill Title"
+        billTitle.borderStyle = .none
         
         view.addSubview(billTitle)
         billTitle.translatesAutoresizingMaskIntoConstraints = false
         
   
-        billTitle.topAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: 30).isActive = true // 16pt gap
+        billTitle.topAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: 30).isActive = true
         billTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32).isActive = true
         
         //subtext
@@ -101,7 +113,7 @@ class ViewController: UIViewController {
         
         view.addSubview(amount)
         amount.translatesAutoresizingMaskIntoConstraints = false
-        amount.topAnchor.constraint(equalTo: billTitle.bottomAnchor, constant: 30).isActive = true // 16pt gap
+        amount.topAnchor.constraint(equalTo: billTitle.bottomAnchor, constant: 30).isActive = true
         amount.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32).isActive = true
         
         //splitby
@@ -112,7 +124,7 @@ class ViewController: UIViewController {
         
         view.addSubview(splitBy)
         splitBy.translatesAutoresizingMaskIntoConstraints = false
-        splitBy.topAnchor.constraint(equalTo: billTitle.bottomAnchor, constant: 30).isActive = true // 16pt gap
+        splitBy.topAnchor.constraint(equalTo: billTitle.bottomAnchor, constant: 30).isActive = true
         splitBy.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 240).isActive = true
         //text field
         view.addSubview(numField)
@@ -120,9 +132,25 @@ class ViewController: UIViewController {
         numField.heightAnchor.constraint(equalToConstant: 35).isActive = true
         numField.topAnchor.constraint(equalTo: amount.bottomAnchor, constant: 8).isActive = true
         numField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32).isActive = true
+        
+        //percent
+        percent.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        percent.layer.cornerRadius = 5.86
+        percent.setTitle("%", for: .normal)
+        percent.setTitleColor(UIColor(red: 0.557, green: 0.557, blue: 0.576, alpha: 1), for: .normal)
+        percent.titleLabel?.font = UIFont.systemFont(ofSize: 16)
 
-        
-        
+        // pressed state
+        percent.addTarget(self, action: #selector(toggleButton), for: .touchUpInside)
+
+        view.addSubview(percent)
+        percent.translatesAutoresizingMaskIntoConstraints = false
+        percent.widthAnchor.constraint(equalToConstant: 44.95).isActive = true
+
+        percent.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        percent.topAnchor.constraint(equalTo: splitBy.bottomAnchor, constant: 8).isActive = true
+        percent.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 240).isActive = true
+       
         
         
         
@@ -140,10 +168,9 @@ class ViewController: UIViewController {
             backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
                         backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
                         
-                        // Fixed height of 23.57 points
                         backgroundView.heightAnchor.constraint(equalToConstant: 40.07),
                         
-                        // Pin to very top of the screen (under status bar)
+                        // under status bar
                         backgroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
            ])
        }
